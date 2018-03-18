95bb14439330bcc8aebbf9908acc1e837a4e65c3
ruksana24 <rukshad.cse@gmail.com>
2018-03-18 19:52:02 +0600

    updated

--- editContact.php
+++ editContact.php
@@ -54,6 +54,7 @@
     <div class="form-group">
       <label class="control-label col-sm-2" for="fname">Full Name:</label>
       <div class="col-sm-10">
+
         <input type="text" class="form-control" id="fname" placeholder="Enter Full Name" name="fname" value="<?php echo $row['fullName'] ?>">
       </div>
     </div>
@@ -96,7 +97,7 @@
     
     <div class="form-group">        
       <div class="col-sm-offset-2 col-sm-10">
-        <button type="submit" class="btn btn-default">Submit</button>
+        <button type="submit" class="btn btn-primary">Submit</button>
       </div>
     </div>
   </form>
